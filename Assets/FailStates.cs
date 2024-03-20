44fce016fe68ebbf055f95ef09f1f66c456d97a2
Alec <144955718+alecxanderian@users.noreply.github.com>
2024-03-20 16:47:23 -0400

    added bounding and fuel feature

---
+++ Assets/FailStates.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using Calculator;
+using ControlSystem;
+using Unity.VisualScripting;
+using UnityEditor.Callbacks;
+using UnityEditor.SearchService;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class FailStates : MonoBehaviour
+{
+    public Rigidbody rb;
+    private Keyboard controlKF;
+    private GeneralCalculations calc;
+    public float fuel;
+    public float alt;
+    public float roll;
+    public float pitch;
+    public float yaw; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        controlKF = GetComponent<Keyboard>(); // Replace 'ControlSystem' with the appropriate class name from the 'Calculator' namespace.
+        calc = GetComponent<GeneralCalculations>();
+
+        roll = Input.GetAxis("Roll");
+        pitch = Input.GetAxis("Pitch");
+        yaw = Input.GetAxis("Yaw");
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        alt = calc.getAlt();
+
+
+        if (alt >= 50000f) {
+//          add force to bound downwards
+            Debug.Log("ship has reached bounds");
+        }
+        else if (alt <= -99999f & rb.velocity.y <= 0){
+            // gameover scene pause game
+            Debug.Log("ship has reached ground");
+        }
+        if (fuel <= 0) {
+            Debug.Log("You are out of fuel");
+            //stop forces
+        }
+
+    }
+}
